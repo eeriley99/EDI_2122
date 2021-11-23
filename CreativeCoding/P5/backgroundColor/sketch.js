99bf73e34d6b8017bddd04a49019ecf0d168f0a7
eeriley99 <eriley@greenwichacademy.org>
2021-11-22 21:13:51 -0500

    updates

--- CreativeCoding/P5/backgroundColor/sketch.js
+++ CreativeCoding/P5/backgroundColor/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
 createCanvas (windowWidth, windowHeight);//this creates the window W/H to be the size of my browser window
-background (255,255,0);//red background
+background (255,0,255);//red background
 }
 
 function draw() {
