20d85f785fe578cf0f589d6296e5c10b9859bd98
eeriley99 <eriley@greenwichacademy.org>
2021-11-29 22:50:54 -0500

    AiBG

--- CreativeCoding/P5/Ai_BG/sketch.js
+++ CreativeCoding/P5/Ai_BG/sketch.js
@@ -7,28 +7,21 @@ function preload() {
 
 function setup() {
   createCanvas (windowWidth, windowHeight);
-  background (255, 0, 0);
-  image(img, 0, 0);
+  background (255, 0, 255);
+  noFill ();
+
+
 }
 
 function draw (){
-  translate (60,60);
-  //For loop parts:
-  //1)declare and initialize the increment variable
-  //2)test expression
-  //3)increment
-    for (var y = 0; y < 600; y = y + 300) {//loop to create rows in the y direction
 
-       for (var x = 0; x < 600; x = x + 300) {// loop to create a row of squares in the x direction
 
-    quad(x, y,
-      x + 300, y,
-      x + 300, y + 300,
-      x, y + 300);
+  for (var y = 0; y < 1800; y = y + 150){
+      for (var x = 0; x < 1800; x = x + 600){
+        image(img, x, y);
+      }
+}
 
-    }
 
-    }
-
-    noLoop ();
+  noLoop ();
 }
