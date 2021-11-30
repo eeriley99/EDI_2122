973d2ce5a1e22ad44eace8d29a84054511874675
eeriley99 <eriley@greenwichacademy.org>
2021-11-29 22:53:15 -0500

    Update sketch.js

--- CreativeCoding/P5/Ai_BG/sketch.js
+++ CreativeCoding/P5/Ai_BG/sketch.js
@@ -9,19 +9,14 @@ function setup() {
   createCanvas (windowWidth, windowHeight);
   background (255, 0, 255);
   noFill ();
-
-
 }
 
 function draw (){
-
-
   for (var y = 0; y < 1800; y = y + 150){
       for (var x = 0; x < 1800; x = x + 600){
         image(img, x, y);
       }
 }
-
 
   noLoop ();
 }
