fc7a960f9f4a1016e1df0820c64b6968601d728d
eeriley99 <eeriley99@gmail.com>
2021-11-30 23:03:25 -0500

    tuning variables

---
+++ CreativeCoding/P5/TuneVariablesGIF/sketch.js
@@ -0,0 +1,30 @@
+let gif;//declare image variable
+
+function preload (){
+gif = loadImage ('assets/AiBG75.gif');
+}
+
+function setup() { //runs once
+  createCanvas(windowWidth, windowHeight);
+  fill('yellow');
+  strokeWeight(1);
+}
+
+function draw() {
+
+translate (-100, -100);
+
+var num = 10; //variable for the number of squares in the grid
+var sideLen = windowWidth/num; //variable for the side length of each square
+
+  for (var y = 0; y < windowHeight + 100; y = y + sideLen) {//loop to create rows in the y direction
+     for (var x = 0; x < windowWidth + 100; x = x + sideLen) {// loop to create a row of squares in the x direction0
+image (gif, x, y);
+  }
+  }
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
