e506234a34b1b67d646278e1e8ceb3f9b24a3a1a
eeriley99 <eeriley99@gmail.com>
2021-11-30 22:11:19 -0500

    remove noLoop

--- CreativeCoding/P5/Troubleshooting11_30/sketch.js
+++ CreativeCoding/P5/Troubleshooting11_30/sketch.js
@@ -23,14 +23,10 @@ function draw() {
 
 
 translate (60,60);
-//For loop parts:
-//1)declare and initialize the increment variable
-//2)test expression
-//3)increment
-  for (var y = 0; y < 600; y = y + 300) {//loop to create rows in the y direction
-     for (var x = 0; x < 600; x = x + 300) {// loop to create a row of squares in the x direction0
-    image (gif, x, y);
+for (var y = 0; y < 600; y = y + 300){
+  for (var x = 0; x < 600; x = x + 300) {
+  image (gif, x, y);
   }
-  }
-  noLoop ();
+}
+
 }
