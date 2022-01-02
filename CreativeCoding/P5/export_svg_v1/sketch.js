9acddb875256ea0e3303d5d5ed80bdbd2291a88d
eeriley99 <eeriley99@gmail.com>
2022-01-01 22:35:43 -0500

    Update sketch.js

--- CreativeCoding/P5/export_svg_v1/sketch.js
+++ CreativeCoding/P5/export_svg_v1/sketch.js
@@ -1,22 +1,15 @@
 /* export SVG
 */
-let ix = 24; //paper width in inches
-let iy = 18; //paper height in inches
-
-let canvaWidth = ix * 72; //72 pixels per inch(digital resolution)
-let canvaHeight = iy * 72;
-
 function setup() {
-  createCanvas(canvaWidth, canvaHeight, SVG); // Create SVG Canvas
+  createCanvas(200, 200, SVG); // Create SVG Canvas
   strokeWeight(1); // do 0.1 for laser
-  stroke(0); // red is good for laser
+  stroke(255, 0, 0); // red is good for laser
   noFill(); // better not to have a fill for laser
 }
 
 function draw() {
-  background (0, 255, 0);
-  for (x = 0; x < canvaWidth; x++) {
-    for (y = 0; y < canvaHeight; y++) {
+  for (x = 0; x < 10; x++) {
+    for (y = 0; y < 10; y++) {
       rect(x * 10, y * 10, x, y);
     }
   }
