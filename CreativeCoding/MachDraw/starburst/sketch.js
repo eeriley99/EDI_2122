dca43bbce0caa4fcb30e345d68f1f5c41f5de2d3
eeriley99 <53475374+eeriley99@users.noreply.github.com>
2022-01-03 20:38:00 -0500

    starburst

---
+++ CreativeCoding/MachDraw/starburst/sketch.js
@@ -0,0 +1,23 @@
+
+function setup() {
+  createCanvas(720, 400);
+  background(230);
+  frameRate (10);
+}
+
+function draw() {
+
+  if (mouseIsPressed) {
+    stroke(0);
+    strokeWeight(5);
+      line (width/2, height/2, mouseX, mouseY);
+
+    }
+
+    if (keyIsPressed === true) {
+        save("starburst.png"); // give file name
+        print("saved png");
+        noLoop ();
+    }
+
+}
