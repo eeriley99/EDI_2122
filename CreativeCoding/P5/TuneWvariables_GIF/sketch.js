d37a32960387a46c4732e6285703ad67f1ede6a1
eeriley99 <eeriley99@gmail.com>
2023-01-09 00:55:22 -0500

    update

--- CreativeCoding/P5/TuneWvariables_GIF/sketch.js
+++ CreativeCoding/P5/TuneWvariables_GIF/sketch.js
@@ -17,9 +17,9 @@ background (255);
 var num = 15; //variable for the number of squares in the array
 var sideLen = windowWidth/num;//variable for the side lenth of each square
 
-  for (var y = 0; y < windowHeight; y = y + sideLen) {//loop to create rows in the y direction
-     for (var x = 0; x < windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
-image (gif, x, y);
+  for (var y = 0; y < 2 * windowHeight; y = y + sideLen) {//loop to create rows in the y direction
+     for (var x = 0; x < 2 * windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
+image (gif, x, y, windowWidth/num,windowWidth/num););
   }
 }
 
