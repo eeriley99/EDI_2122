06cf30b22877a08416ad516c83a337c2d4a5de9a
Erin E. Riley <53475374+eeriley99@users.noreply.github.com>
2023-03-06 21:43:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,12 +14,12 @@ function draw() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight * 2);
+  resizeCanvas(windowWidth * 2, windowHeight * 2);
 }
 
 function squ() {
   for (var y = 0; y < windowHeight * 2; y = y + 120){
-     for (var x = 0; x < windowWidth; x = x + 120){
+     for (var x = 0; x < windowWidth * 2; x = x + 120){
      square(80 + x, random (10, 90) + y, random (10, 40));
 }
   }
@@ -29,7 +29,7 @@ function squ() {
   function zigzag() {
     translate(20, 0);
     for (var y = 0; y < windowHeight * 2; y = y + 120) {
-      for (var x = 0; x < windowWidth; x = x + 120) {
+      for (var x = 0; x < windowWidth * 2; x = x + 120) {
 
         line(random (0, 20) + x, random (0, 20) + y, x + 40, y + 40); //1st diagonal
 
